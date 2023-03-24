8291d6ffec6cf98b8e29a3509f8091c8618d13df
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2023-03-23 21:47:11 -0500

    feat: :sparkles: Dinamico
    Uso de directiva

--- component/myimg.js
+++ component/myimg.js
@@ -1,7 +1,9 @@
 export default{
     data(){
         return {
+            atributo: "src",
             Imagen: "https://i.scdn.co/image/ab6761610000e5ebeb27e0d833dcc24c376cb24c" , 
+            atributo2: "alt",
             texto: "Diferencias entre vue2 y vue3   "
        }
     }
