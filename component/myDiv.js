2ea193414302c13d809cfaee2a8a3fd2f7097dc4
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2023-03-23 23:21:07 -0500

    feat: :sparkles: Style
    Utilizando directivas

--- component/myDiv.js
+++ component/myDiv.js
@@ -1,13 +1,16 @@
 export default{
     data(){
         return {
-            className: "box",
-            className2: ["box", "box2"],
-            className3:{
-                box:true,
-                box2:false,
-                box3:true
+            styleName:"background: red",
+            styleName2: ["background: #a1a1a1", "color:#fff"],
+            styleName3:{
+                background:"violet",
+                color: "#fff",
+                "font-size": "sans-serif",
+                padding: "1rem",
+                "border-radius":".5rem",
+                margin:".5rem o"
+            }
             }
         }
     }
-}
