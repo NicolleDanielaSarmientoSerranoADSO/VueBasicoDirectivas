ef44e6384bc5c2b9fa098e5f9401943509f8abd1
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2023-03-23 21:35:17 -0500

    feat: :sparkles: v-bind
    Esta directiva permite enlazar una variable Vue con un atributo especifico de una etiqueta html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import { createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-import mycomponent from './component/my.component.js'
+import mycomponent from './component/myimg.js'
 
 createApp(mycomponent).mount('#app')
---
+++ component/myimg.js
@@ -0,0 +1,8 @@
+export default{
+    data(){
+        return {
+            Imagen: "https://i.scdn.co/image/ab6761610000e5ebeb27e0d833dcc24c376cb24c" , 
+            texto: "Diferencias entre vue2 y vue3   "
+       }
+    }
+}
